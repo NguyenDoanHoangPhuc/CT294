8b0bec11579176cc221f0791f5fa4e198c17142a
HoangPhuc-edward <hoangphuc090104@gmail.com>
2024-10-04 19:45:15 +0700

    Add auto-loop clustering code

--- phuc/clustering_phuc.py
+++ phuc/clustering_phuc.py
@@ -12,6 +12,9 @@ import numpy as np
 import matplotlib.patches as mpatches
 from pyvis.network import Network
 from scipy.stats import chi2_contingency
+from sklearn.metrics import silhouette_score
+
+from SimilarityGraph import SimilarityGraph
 
 # Hàm để tính khoảng cách polar giữa hai chuỗi hành động
 def polar_distance(list1, list2, n):
@@ -56,34 +59,6 @@ def polar_distance(list1, list2, n):
     polar_distance_result = (1 / np.pi) * np.arccos(cosine_similarity)
     return polar_distance_result
 
-# Hàm tạo đồ thị từ dữ liệu
-def create_graph(table_data, distance_function, threshold, attributes):
-    G = nx.Graph()
-
-
-    for sessionID in table_data['session ID']:
-        G.add_node(sessionID)
-
-    for idx1, row1 in table_data.iterrows():
-        session_id1 = row1['session ID']
-
-        for idx2, row2 in table_data.iterrows():
-            session_id2 = row2['session ID']
-            if (session_id2 <= session_id1):
-                continue
-            sum = 0
-            for attribute in attributes:
-                clothing_models1 = tuple(row1[attribute])
-                clothing_models2 = tuple(row2[attribute])
-                sum += distance_function(clothing_models1, clothing_models2, 2)
-            average_distance = sum / len(attributes)
-            if average_distance < threshold:
-                G.add_edge(session_id1, session_id2, weight=average_distance)                
-                    
-        
-    return G
-
-
 #Hàm chuyển Partition thành Clusters
 def turn_to_clusters(partition):
     clusters = {}
@@ -93,45 +68,8 @@ def turn_to_clusters(partition):
             clusters[cluster_id] = []
         clusters[cluster_id].append(session_id)
 
-    return clusters
-
-# Hàm vẽ đồ thị
-def show_graph(clusters, G, id, showLegend = True, showEdges = True):
-    cmap = matplotlib.colormaps.get_cmap('tab20')
-
-    color_map = {}
-    color_index = 0
-
-    # Tạo danh sách chú thích (legend)
-    legend_items = []
-
-    for cluster_id, nodes in clusters.items():
-        if len(nodes) == 1:
-            # Cụm chỉ có 1 phần tử -> gán màu xanh dương
-            for node in nodes:
-                color_map[node] = 'white'
-        else:
-            # Cụm có nhiều phần tử -> gán màu từ bảng màu
-            current_color = cmap(color_index)
-            for node in nodes:
-                color_map[node] = current_color
-            color_index += 1
-            legend_items.append(mpatches.Patch(color=current_color, label=f"Cluster {cluster_id}"))  # Chú thích cho cụm có nhiều phần tử
-
-    # Lấy danh sách màu cho các node trong đồ thị
-    node_colors = [color_map[node] for node in G.nodes()]
-
-    # Vẽ đồ thị
-    pos = nx.spring_layout(G)  # Layout cho đồ thị
-    
-    if showEdges:
-        nx.draw(G, pos, node_color=node_colors, node_size=50)
-    else:
-        nx.draw_networkx_nodes(G, pos, node_color=node_colors, node_size=50)
-    # Thêm chú thích
-    plt.legend(handles=legend_items, loc="upper right", title="Cluster")
-    plt.title(f'Graph {id}')
-    plt.show()
+    filtered_clusters = {cluster_id: members for cluster_id, members in clusters.items() if len(members) > 1}
+    return filtered_clusters
 
 def analyze_cluster(cluster, dataset):
     # Lọc các dòng trong dataset có sessionID thuộc cụm
@@ -171,7 +109,6 @@ def analyze_cluster(cluster, dataset):
     print(results_df)
     return labels
 
-
 def label(attribute, value):
     if attribute == 'page 1 (main category)':
         name = ['trousers', 'skirts', 'blouses', 'sale']
@@ -195,94 +132,119 @@ def label(attribute, value):
     else:
         return 'Unknown attribute'
 
+def clustering(graph):
+    partitions = community_louvain.best_partition(graph)
+    labels = [partitions[node] for node in graph.nodes()]
+    
+    if len(set(labels)) >= 2:  # Make sure there are at least 2 clusters
+        silhouette = silhouette_score(nx.to_numpy_array(graph), labels)
+    else:
+        silhouette = -1  # Invalid silhouette score if only 1 cluster exists
 
+    return partitions, silhouette
+
+def remove_attribute(attributes, graph, data):
+    sessionID_list = list(graph.nodes())
+    filtered_df = data[data['session ID'].isin(sessionID_list)]
+    filtered_columns_df = filtered_df.loc[:, 'page 1 (main category)': 'page']
+    filtered_columns_df['cluster'] = data['session ID'].map(partitions)
+                
+    for attribute in attributes:
+        contingency = pd.crosstab(filtered_columns_df['cluster'], filtered_columns_df[attribute])
+        chi2, p_value, dof, expected = chi2_contingency(contingency)
+        if p_value > 0.05 and len(attributes) > 1:
+             attributes.remove(attribute)
+    return attributes                 
 
 # Đọc dữ liệu từ CSV
 df = pd.read_csv('e-shop clothing 2008.csv', sep = ';')
 
 # Lấy ra n dòng đầu tiên từ dữ liệu cho trước
-data = df.head(n = 7078)
+data = df.head(n = 2038)
 
-# Nhóm dữ liệu theo session ID, nó trả về DataFrame bao gồm nhiều cột
-# Từ DataFrame ta lấy cột clothing model, biến nó thành list bằng apply()
-# Từ List, ta biến nó trở thành DataFrame
-# grouped_clothingmodel = data.groupby('session ID')['page 2 (clothing model)'].apply(list).reset_index()
+default_attributes = ['page 2 (clothing model)', 'colour', 'location', 'model photography', 'price 2', 'page']
 
-# Lọc dữ liệu, chọn những chuỗi có độ dài tối thiểu là 2
-# filtered_grouped = grouped_clothingmodel[grouped_clothingmodel['page 2 (clothing model)'].apply(lambda x: len(x) > 2)]
-
-# Danh sách thuộc
-# attributes = ['page 2 (clothing model)', 'colour', 'location', 'model photography', 'price 2', 'page']
-# attributes = ['page 2 (clothing model)', 'colour', 'model photography', 'page']
-# attributes = ['page 2 (clothing model)', 'model photography', 'page']
-attributes = ['model photography', 'page']
-# attributes = ['page 2 (clothing model)', 'page']
-# attributes = ['model photography']
-
-columns_name = ['session ID'] + attributes 
-grouped = pd.DataFrame(columns = columns_name)
-for idx, attribute in enumerate(attributes):
+main_columns_name = ['session ID'] + default_attributes 
+main_grouped = pd.DataFrame(columns = main_columns_name)
+for idx, attribute in enumerate(default_attributes):
     temp = data.groupby('session ID')[attribute].apply(list).reset_index()
     filtered_temp = temp[temp[attribute].apply(lambda x: len(x) > 2)]
-    grouped[attribute] = filtered_temp[attribute]
-    grouped['session ID'] = filtered_temp['session ID']
+    main_grouped[attribute] = filtered_temp[attribute]
+    main_grouped['session ID'] = filtered_temp['session ID']
+
+sGraph = SimilarityGraph(main_grouped, 0, default_attributes, polar_distance)
 
 
-# Tạo đồ thị từ dữ liệu
-G = create_graph(grouped, polar_distance, 0.5, attributes)
+nodes_list = []
+nodes_list.append(data['session ID'].tolist())
+time_count = 0
 
-# # Tìm tất cả các thành phần liên thông
-connected_components = list(nx.connected_components(G))
+while (time_count <= 0):
+    nodes_list2 = []
+    print("Time count: ", time_count)
 
-# # Lọc ra những bộ phận liên thông có nhiều hơn 2 đỉnh
-filtered_components = [component for component in connected_components if len(component) > 2]
+    for nodes in nodes_list:
+        # Tao nhung thuoc tinh can thiet cho qua trinh phan cum
+        threshold = 0
+        attributes = default_attributes.copy()
+        # Bien kiem soat xem vong lap phan cum chay bao nhieu lan
+        cluster_loop_count = 0
+        # Co bien kiem soat de thoat vong lap
+        break_flag = False
+        
+        best_silhouette = -100
+        best_partitions = None
 
-# # Tạo các đồ thị con từ mỗi thành phần liên thông
-sub_graphs = [G.subgraph(component).copy() for component in filtered_components]
+        while cluster_loop_count < 80 and not break_flag:
+            cluster_loop_count += 1
+            # Tao bang du lieu tu dataset da duoc loc
+            columns_name = ['session ID'] + attributes
+            grouped = pd.DataFrame(columns = columns_name)
 
+            # Them du lieu vao bang grouped
+            for idx, attribute in enumerate(attributes):
+                temp = data.groupby('session ID')[attribute].apply(list).reset_index()
+                temp = temp[temp['session ID'].isin(nodes)]
+                filtered_temp = temp[temp[attribute].apply(lambda x: len(x) > 2)]
+                grouped[attribute] = filtered_temp[attribute]
+                grouped['session ID'] = filtered_temp['session ID']
 
-# cluster_values = [4, 8, 12, 15, 17, 18, 20, 23, 24, 31, 33, 44, 49, 50, 57, 64, 67, 69, 70, 76, 79, 84, 94, 99, 107, 110, 116, 123, 143, 144, 146, 148, 157, 160, 164, 165, 170, 173, 174, 177, 179, 181, 184, 186, 187, 188, 192, 193, 194, 211, 212, 214, 215, 217, 220, 222, 225, 226, 232, 233, 236, 238, 239, 242, 244, 247, 248, 249, 253, 264, 273, 275, 276, 278, 279, 282, 287, 295, 301, 304, 314, 316, 320, 321, 324, 325, 327, 331, 332, 334, 335, 336, 337, 342, 343, 344, 347, 350, 364, 372, 375, 381, 383, 384, 386, 391, 395, 414, 416, 417, 420, 422, 424, 435, 437, 441, 443, 454, 459, 460, 463, 464, 466, 469, 471, 476, 477, 479, 480, 485, 486, 488, 492, 493, 497, 501, 506, 507, 512, 513, 514, 518, 522, 525, 529, 531, 533, 537, 539, 542, 544, 547, 550, 553, 554, 563, 568, 572, 574, 583, 584, 591, 594, 597, 600, 603, 604, 606, 607, 608, 610, 615, 621, 623, 624, 628, 629, 631, 641, 645, 648, 650, 659, 660, 676, 677, 681, 690, 694, 695, 702, 708, 718, 719, 720, 722, 727, 735, 739, 744, 746, 751, 757, 764, 769, 772, 774, 779, 795, 800, 802, 803, 810, 811, 815, 822, 827, 828, 829, 830, 836, 837, 838, 842, 844, 845, 847, 848, 849]
-# sub_graphs = [G.subgraph(cluster_values).copy()]
+            # Tu bang grouped, ta tao duoc do thi tuong dong tu lop SimilarityGraph
+            similarityGraph = SimilarityGraph(grouped, threshold, attributes, polar_distance)
 
+            result = clustering(similarityGraph.graph)
+            partitions = result[0]
+            silhouette_louvain = result[1]
 
-# Đưa các đồ thị liên thông vào, tiến hành gom cụm cho từng đồ thị
-for i, sub_graph in enumerate(sub_graphs):
-    partitions = community_louvain.best_partition(sub_graph)
+            if silhouette_louvain > best_silhouette:
+                best_silhouette = silhouette_louvain
+                best_partitions = partitions
 
-    cluster_list = []
-    for cluster_id in partitions.values():
-        cluster_list.append(cluster_id)
+            # Neu chi so silhouette phu hop thi them vao nodes_list2, ket thuc vong lap
+            if silhouette_louvain >= 0.7:
+               
+                clusters = turn_to_clusters(partitions)
+                for cluster_id, cluster in clusters.items():
+                    nodes_list2.append(cluster)
+                break_flag = True
+            # Neu chi so silhouette khong phu hop, ma khong co thuoc tinh nao de loai thi ta giam nguong threshold
+            elif len(attributes) <= 1:
+                threshold += 0.05
+                attributes = default_attributes.copy()
+            # Neu chi so silhouette khong phu hop, ta loai bo thuoc tinh khong phu hop
+            else:
+                removed = remove_attribute(attributes, similarityGraph.graph, data)
+                if len(removed) == 0:
+                    threshold += 0.05
+                    attributes = default_attributes.copy()     
+        if not break_flag:
+            clusters = turn_to_clusters(best_partitions)
+            for cluster_id, cluster in clusters.items():
+                nodes_list2.append(cluster)
     
-    sessionID_list = list(sub_graph.nodes())
-    filtered_df = df[df['session ID'].isin(sessionID_list)]
-    filtered_columns_df = filtered_df.loc[:, 'page 1 (main category)': 'page']
-    filtered_columns_df['cluster'] = df['session ID'].map(partitions)
-    
-    for attribute in attributes:
-        contingency = pd.crosstab(filtered_columns_df['cluster'], filtered_columns_df[attribute])
-        chi2, p_value, dof, expected = chi2_contingency(contingency)
-        print(f"{attribute}: Chi-Square: {chi2}, p-value: {p_value}") # < 0.05
-    
-    clusters = turn_to_clusters(partitions)
-    show_graph(clusters, sub_graph, f"Big Graph {i}", showEdges=False)
-    # for id, cluster in clusters.items():
-    #     if len(cluster) > 1:
-            # sub_graph = G.subgraph(cluster).copy()
-            # partitions = community_louvain.best_partition(sub_graph)
-            # cluster_list = []
-            # for cluster_id in partitions.values():
-            #     cluster_list.append(cluster_id)
-            
-            # sessionID_list = list(sub_graph.nodes())
-            # filtered_df = df[df['session ID'].isin(sessionID_list)]
-            # filtered_columns_df = filtered_df.loc[:, 'page 1 (main category)': 'page']
-            # filtered_columns_df['cluster'] = df['session ID'].map(partitions)
-            # mini_atti
+    nodes_list = nodes_list2.copy()
+    time_count += 1
 
-            # for attribute in attributes:
-            #     contingency = pd.crosstab(filtered_columns_df['cluster'], filtered_columns_df[attribute])
-            #     chi2, p_value, dof, expected = chi2_contingency(contingency)
-            #     print(f"{attribute}: Chi-Square: {chi2}, p-value: {p_value}")
-            
-            # clusters = turn_to_clusters(partitions)
-            # show_graph(clusters, sub_graph, f"Big Graph {i}", showEdges=False)
+
+
+sGraph.show_graph(nodes_list, showLegend = True, showEdges = False, showWeights=False)
